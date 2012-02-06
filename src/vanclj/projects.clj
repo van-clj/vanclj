a1002e8af25768e13deb0472976c671a1848f69c
Roman Gonzalez <romanandreg@gmail.com>
2012-02-06 14:03:18 -0800

    Adding zetta-parser to the list of projects

--- src/vanclj/projects.clj
+++ src/vanclj/projects.clj
@@ -1,10 +1,10 @@
 (def projects
   { 
    
-   ;"zetta-parser" {
-   ;   :url "http://github.com/vancloj/zetta-parser"
-   ;   :summary "A monadic parser combinator library in Clojure (port of Haskell's attoparsec)."
-   ; }
+   "zetta-parser" {
+      :url "http://github.com/van-clj/zetta-parser"
+      :summary "A monadic parser combinator library in Clojure (port of Haskell's attoparsec)."
+    }
 
    "river" {
      :url "http://github.com/roman/river"
@@ -16,8 +16,8 @@
    ;   :summary "Transforms a river stream from raw input into parsed input using zetta parsers."
    ; }
 
-   ; "plusplus-lingrbot" {
-   ;   :url "https://github.com/van-clj/plusplus-lingrbot"
-   ;   :summary "Counts number of ++ on lingr."
-   ; }
+   "plusplus-lingrbot" {
+     :url "https://github.com/van-clj/plusplus-lingrbot"
+     :summary "Counts number of ++ on lingr."
+   }
   })
