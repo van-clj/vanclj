3835c2055d7eb826e9340061b18eb520d167a1aa
Roman Gonzalez <romanandreg@gmail.com>
2012-02-19 17:30:52 -0800

    Adding http-river and zetta-river to the list of projects

--- src/vanclj/projects.clj
+++ src/vanclj/projects.clj
@@ -1,14 +1,14 @@
 (def projects
-  { 
-   
+  {
+
    "zetta-parser" {
       :url "http://github.com/van-clj/zetta-parser"
       :summary "A monadic parser combinator library in Clojure (port of Haskell's attoparsec)."
     }
-   
+
    "json-zetta" {
-     :url "http://github.com/van-clj/json-zetta" 
-     :summary "JSON zetta-parser combinator (port of Haskell's aeson parser)"
+     :url "http://github.com/van-clj/json-zetta"
+     :summary "JSON zetta-parser combinator (port of Haskell's aeson parser)."
    }
 
    "river" {
@@ -16,10 +16,15 @@
      :summary "A monadic stream library in Clojure (port of Haskell's enumerator)."
    }
 
-   ; "zetta-river" {
-   ;   :url "http://github.com/vancloj/zetta-river"
-   ;   :summary "Transforms a river stream from raw input into parsed input using zetta parsers."
-   ; }
+   "zetta-river" {
+     :url "http://github.com/vancloj/zetta-river"
+     :summary "Transforms a river stream from raw input into parsed input using zetta parsers."
+   }
+
+   "http-river" {
+     :url "https://github.com/van-clj/http-river"
+     :summary "Stream HTTP response bodies with river."
+   }
 
    "plusplus-lingrbot" {
      :url "https://github.com/van-clj/plusplus-lingrbot"
